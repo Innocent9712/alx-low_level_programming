1d8d0bebf06c5b24c5de51574d3014de903531b7
Innocent Chukwuemeka <chukwuemeka140@gmail.com>
2022-03-15 05:41:23 -0700

    Modify main.h and add task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet a-z followed by a new line 10 times
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	int x;
+	char c;
+
+	x = 0;
+	while (x < 10)
+	{
+		c = 'a';
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+		_putchar('\n');
+		x++;
+	}
+}
