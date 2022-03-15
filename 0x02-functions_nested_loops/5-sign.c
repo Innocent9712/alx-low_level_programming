7430b89f1b9f917e9155fd09faa80937a1179e80
Innocent Chukwuemeka <chukwuemeka140@gmail.com>
2022-03-15 07:26:21 -0700

    Add task 5 and modify main.h

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_sign - checks if input is a positive, negative or zero
+ * @c : parameter for the function ASCII value
+ *
+ * Description: It receives an input and checks if its value.
+ *
+ * Return: 1 (success) 0 (failure)
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
