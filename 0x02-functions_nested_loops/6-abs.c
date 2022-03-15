63ed2181d435698fa483092224cd8b4495f0535a
Innocent Chukwuemeka <chukwuemeka140@gmail.com>
2022-03-15 07:36:37 -0700

    Add task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _abs - prints the absolute value of an input
+ * @num : parameter for the function ASCII value
+ *
+ *
+ * Return: 1 (success) 0 (failure)
+ */
+
+int _abs(int num)
+{
+	return (abs(num));
+}
