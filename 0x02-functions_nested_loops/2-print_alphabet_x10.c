aa333d3f3ee330da992d46c4ac46c3eaf5fed260
Innocent Chukwuemeka <chukwuemeka140@gmail.com>
2022-03-15 05:43:00 -0700

    Fix betty standard documentation

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet - prints the alphabet a-z followed by a new line 10 times
+ * print_alphabet_x10 - prints the alphabet a-z followed by a new line 10 times
  *
  */
 
