b8ff3d93fee40baf5512e785adf526728e3d05b3
Innocent Chukwuemeka <chukwuemeka140@gmail.com>
2022-03-15 04:56:15 -0700

    Modify file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 /**
  * main - prints to string
  *
@@ -9,7 +9,15 @@
 
 int main(void)
 {
-	printf("_putchar\n");
+	_putchar("_");
+	_putchar("p");
+	_putchar("u");
+	_putchar("t");
+	_putchar("c");
+	_putchar("h");
+	_putchar("a");
+	_putchar("r");
+	_putchar("\n");
 
 	return (0);
 }
