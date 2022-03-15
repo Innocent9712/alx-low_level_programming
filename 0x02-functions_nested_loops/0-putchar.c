ffe33ac9e4ab22e3b3161d8e36f3baf548db292a
Innocent Chukwuemeka <chukwuemeka140@gmail.com>
2022-03-15 03:45:11 -0700

    Add New directory with README and task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - prints to string
+ *
+ * Description: Prints "_putchar" to output.
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+
+	return (0);
+}
