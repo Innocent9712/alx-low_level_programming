aaf7c273e99da277e30849a435828dea4bef13a6
Innocent Chukwuemeka <chukwuemeka140@gmail.com>
2022-03-15 09:07:39 -0700

    Add task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - prints the sum of two values
+ * @num1: first value
+ * @num2: second value
+ *
+ * Return: sum of num1 and num2
+ */
+
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -8,3 +8,4 @@ int _abs(int num);
 int print_last_digit(int num);
 void jack_bauer(void);
 void times_table(void);
+int add(int num1, int num2);
