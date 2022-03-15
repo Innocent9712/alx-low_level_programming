290f976e9ff957b9c452ca17689999a9fecb3401
Innocent Chukwuemeka <chukwuemeka140@gmail.com>
2022-03-15 05:19:19 -0700

    Make more changes

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,3 +1,4 @@
+#include <unistd.h>
 #include "main.h"
 /**
  * main - prints to string
@@ -9,15 +10,14 @@
 
 int main(void)
 {
-	_putchar("_");
-	_putchar("p");
-	_putchar("u");
-	_putchar("t");
-	_putchar("c");
-	_putchar("h");
-	_putchar("a");
-	_putchar("r");
-	_putchar("\n");
+	char c[] = "_putchar";
+	int i = 0;
 
+	while(i < 8)
+	{
+		_putchar(c[i]);
+		i++;
+	}
+	_putchar('\n');
 	return (0);
 }
