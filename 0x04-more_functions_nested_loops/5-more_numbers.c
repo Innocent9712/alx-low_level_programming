5435122e2c4e69c09b282b68e3a26fa07bd96f12
Innocent Chukwuemeka <chukwuemeka140@gmail.com>
2022-03-17 05:42:13 -0700

    Add task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * more_numbers - print numbers 0 - 14 ten times
+ *
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	int num, counter;
+
+	counter = 0;
+
+	while (counter < 10)
+	{
+		num = 0;
+		
+		while (num < 14)
+		{
+			_putchar(num + '0');
+			num++;
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
