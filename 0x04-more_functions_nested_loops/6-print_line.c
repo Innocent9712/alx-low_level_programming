89a4e7e31a33bb1e95ed6f925a1b2e0fad574253
Innocent Chukwuemeka <chukwuemeka140@gmail.com>
2022-03-17 06:10:26 -0700

    Add task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_line - print an underscore for n number of times
+ * @n: number of times parameter
+ *
+ * Return: void
+ */
+
+void print_line(int n)
+{
+	while (n >= 0)
+	{
+		if (n > 0)
+		{
+			_putchar('_');
+		}
+		else
+		{
+			_putchar('\n');
+		}
+		n--;
+	}
+}
