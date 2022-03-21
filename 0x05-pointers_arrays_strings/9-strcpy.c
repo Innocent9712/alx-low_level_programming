620010bb90fc67dbb294aab660d2723aaff2a8a4
Innocent Chukwuemeka <chukwuemeka140@gmail.com>
2022-03-21 07:25:41 -0700

    Fix code for this task

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -11,12 +11,11 @@ char *_strcpy(char *dest, char *src)
 {
 	int i;
 
-	i = 0;
-
-	while (src[i] != '\0')
-	{
-		dest[i] = src[i];
+	i = -1;
+	do {
 		i++;
+		dest[i] = src[i];
 	}
+	while (src[i] != '\0')
 	return (dest);
 }
