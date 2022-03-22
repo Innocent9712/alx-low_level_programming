1002c90aa65f35a088cfa6f92ba35cfa54795d44
Innocent Chukwuemeka <chukwuemeka140@gmail.com>
2022-03-22 04:04:13 -0700

    Fix code formatting

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,22 +9,20 @@
 int _atoi(char *s)
 {
 
-        int sign = 1;
-        unsigned int num = 0;
+	int sign = 1;
+	unsigned int num = 0;
 
-        do {
+	do {
 
-                if (*s == '-')
-                        sign *= -1;
+		if (*s == '-')
+			sign *= -1;
+		else if (*s >= '0' && *s <= '9')
+			num = (num * 10) + (*s - '0');
 
-                else if (*s >= '0' && *s <= '9')
-                        num = (num * 10) + (*s - '0');
+		else if (num > 0)
+			break;
 
-                else if (num > 0)
-                        break;
-
-        } while (*s++);
-
-        return (num * sign);
+	} while (*s++);
+	return (num * sign);
 }
 
