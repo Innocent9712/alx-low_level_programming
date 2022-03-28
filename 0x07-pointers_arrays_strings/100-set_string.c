98ee0f0ccf55efabece422346fb071f96db05f5c
Innocent Chukwuemeka <chukwuemeka140@gmail.com>
2022-03-28 06:25:25 -0700

    Advanced task

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+/**
+ * set_string - set the value of a pointer to a char
+ * @s: pointer to be changed
+ * @to: pointer to use
+ *
+ * Return: void
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
