bde9d885944eace82e94fbcddc1a7ec766ab0de9
Innocent Chukwuemeka <chukwuemeka140@gmail.com>
2022-03-30 05:41:41 -0700

    Update changes

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,51 +1,51 @@
 #include "main.h"
+
 /**
- * last_index - returns the last index of a string (counts the null char)
- * @s: pointer the string
- * Return: int
+ * _strlen_recursion - size
+ * @s: pointer to string params
+ * Return: recursion
  */
-int is_palindrome(char *s);
-int check(char *s, int start, int end, int pair);
-int last_index(char *s)
+
+int _strlen_recursion(char *s)
 {
-int n = 0;
-
-if (*s > '\0')
-	n += last_index(s + 1) + 1;
-
-return (n);
+	if (!*s)
+	{
+		return (0);
+	}
+	return (1 + _strlen_recursion(++s));
 }
 
 /**
- * is_palindrome - check if a string is a palindrome
- * @s: string to check
- * Return: 0 or 1
+ * p1 - palindrome
+ * @s: pointer to string
+ * @l: position
+ * Return: boolena
+ */
+
+int p1(char *s, int l)
+{
+	if (l < 1)
+	{
+		return (1);
+	}
+
+	if (*s == *(s + l))
+	{
+		return (p1(s + 1, l - 2));
+	}
+	return (0);
+}
+
+/**
+ * is_palindrome - palindrome
+ * @s: pointer to string
+ * Return: recursion
  */
 
 int is_palindrome(char *s)
 {
-int end = last_index(s);
+	int len = _strlen_recursion(s);
 
-return (check(s, 0, end - 1, end % 2));
+	return (p1(s, len - 1));
 }
 
-/**
- * check - checker for the palindrome
- * @s: string
- * @start: int moves from right to left
- * @end: int moves from left to right
- * @pair: int
- * Return: 0 or 1
- */
-
-
-int check(char *s, int start, int end, int pair)
-{
-
-if ((start == end && pair != 0) || (start == end + 1 && pair == 0))
-	return (1);
-else if (s[start] != s[end])
-	return (0);
-else
-	return (check(s, start + 1, end - 1, pair));
-}
