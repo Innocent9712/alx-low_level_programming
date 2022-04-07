a0befda8a2afa7f0305966590ca57b772908459c
Innocent Chukwuemeka <chukwuemeka140@gmail.com>
2022-04-07 04:42:42 -0700

    Modify file

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -9,23 +9,20 @@
 
 int  *array_range(int min, int max)
 {
-	int i, *arr, j = 0;
+	int i, *arr, j = max - min + 1;
 
 	if (min > max)
 		return (NULL);
 
-	arr = malloc((max - min + 1) * sizeof(*arr));
+	arr = malloc(j * sizeof(*arr));
 
 	if (arr == 0)
 		return (NULL);
 
-	for (i = min; i <= max; i++)
+	for (i = 0; i < j; i++)
 	{
-		arr[j] = i;
-		j++;
+		arr[i] = min++;
 	}
-
-	arr[j] = '\0';
 
 	return (arr);
 }
