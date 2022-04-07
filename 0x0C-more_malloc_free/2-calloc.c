d31078e10082706b866e41dde37a09eda600e196
Innocent Chukwuemeka <chukwuemeka140@gmail.com>
2022-04-07 04:27:12 -0700

    modify file

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -14,7 +14,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	arr = malloc(nmemb * size);
+	arr = malloc(nmemb * sizeof(int));
 
 	if (arr == 0)
 		return (NULL);
@@ -22,7 +22,10 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	i = 0;
 
 	while (*arr + i)
+	{
 		arr[i] = 0;
+		i++;
+	}
 
 	return (arr);
 }
