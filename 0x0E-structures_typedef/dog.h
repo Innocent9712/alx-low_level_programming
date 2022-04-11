0b576c02d649df97b9b1a7a2d4040c0be4c25f5a
Innocent Chukwuemeka <chukwuemeka140@gmail.com>
2022-04-11 04:23:59 -0700

    Make some change

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,4 +16,7 @@ typedef struct dog
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
 #endif /* DOG_H */
