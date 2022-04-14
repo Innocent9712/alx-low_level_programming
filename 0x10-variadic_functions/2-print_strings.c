f1cba2c583497a64d1c0c1111e16e3e07a326951
Innocent Chukwuemeka <chukwuemeka140@gmail.com>
2022-04-14 04:30:07 -0700

    modify code

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -23,7 +23,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			if (val != NULL)
 				printf("%s", val);
 			else
-				printf("nil");
+				printf("(nil)");
 			if (separator != NULL && i != n - 1)
 				printf("%s", separator);
 		}
