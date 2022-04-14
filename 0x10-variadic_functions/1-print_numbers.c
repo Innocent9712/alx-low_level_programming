e0a8e059e68edc661a9832af0a0d820a82bd27e7
Innocent Chukwuemeka <chukwuemeka140@gmail.com>
2022-04-14 04:21:09 -0700

    modify code

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,5 +1,6 @@
 #include "variadic_functions.h"
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * print_numbers - print_numbers : print numbers followed by a new line
  * @separator: character separator
