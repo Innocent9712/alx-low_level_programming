94a279803716f3a044ec02931c87722f429bfb5c
Innocent Chukwuemeka <chukwuemeka140@gmail.com>
2022-05-02 08:54:39 -0700

    Fix bugs

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -42,4 +42,5 @@ int create_file(const char *filename, char *text_content)
 		}
 	}
 	close(file);
+	return (1);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,5 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <elf.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <stdlib.h>
+
 int append_text_to_file(const char *filename, char *text_content);
 int create_file(const char *filename, char *text_content);
 ssize_t read_textfile(const char *filename, size_t letters);
