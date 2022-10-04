708620ce92c9d98ce3c9810b3adf31ae50a54449
Innocent Chukwuemeka <chukwuemeka140@gmail.com>
2022-10-04 11:14:43 +0100

    Add updated header file

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -4,4 +4,10 @@
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
+int advanced_binary(int *array, size_t size, int value);
+/* listint_t *jump_list(listint_t *list, size_t size, int value); */
+/* #skiplist_t *linear_skip(skiplist_t *list, int value); */
 #endif /* SEARCH_ALGOS_H */
