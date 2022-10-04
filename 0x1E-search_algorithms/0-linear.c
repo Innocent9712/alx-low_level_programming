ce91a202fc5645c711adf9efad15337f7305e95b
Innocent Chukwuemeka <chukwuemeka140@gmail.com>
2022-10-04 08:09:57 +0100

    Fix bug

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -13,10 +13,10 @@ int linear_search(int *array, size_t size, int value)
 {
 	size_t i = 0;
 
-	if (!array || size == 0 || !array[size])
+	if (!array)
 		return (-1);
 
-	for (; i <= size; i++)
+	for (; i < size; i++)
 	{
 		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 		if (array[i] == value)
